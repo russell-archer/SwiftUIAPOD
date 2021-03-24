476fd465ba531193c7716204697df329da55fbde
Russell Archer <russell.archer@mac.com>
2021-03-24 18:01:20 +0000

    Initial Commit

---
+++ Shared/SwiftUIAPODApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPODApp.swift
+//  Shared
+//
+//  Created by Russell Archer on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
